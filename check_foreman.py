bfa89f0e21d4af9cf97ab7b128c13461ef489ea2
Michael Süß <michael@intranet.sysmike.de>
2018-08-10 19:09:33 +0200

    Yes, I was being sarcastic.

--- check_foreman.py
+++ check_foreman.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2
 import sys
 import json
 import ssl
 from time import sleep
 from argparse import ArgumentParser
+try:
+    import requests
+except ImportError:
+    print("Please install the python-requests module.")
+    sys.exit(-1)
 
 parser = ArgumentParser(add_help=False)
 parser.add_argument('-H', '--hostname', dest='hostname', metavar='ADDRESS', required=True, help="host name or IP address")
@@ -22,13 +26,13 @@ def results():
 
     while tries >= 0:
         try:
-            url = 'https://{}/api/dashboard'.format(hostname)
-            headers = { 'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0' }
-            req = urllib2.Request(url, None, headers)
-            html = urlopen(req)
-            data = json.loads(html.read())
+
+            url = 'https://{}/api/dashboard'.format(str(args.hostname))
+            r = requests.get(url, auth=(str(args.username), str(args.password)))
+            data = r.json()
+            print(data)
             if data:
-                total_hosts = data['data']['total_hosts']
+                total_hosts = data['total_hosts']
                 break
             else:
                 sleep(15)
@@ -46,12 +50,12 @@ try:
 
     print()
 
-    if averagehashrate < 50:
+    if averagehashrate > 1 :
         sys.exit(2)
     else:
         sys.exit(0)
 
 except Exception as e:
 
-    print e
+    print (e)
     sys.exit(1)
